d1f45dc87dbb262f55d336420fa92cd48d0d4ec5
Jack Tan <jack.ziqi.tan@gmail.com>
2017-06-06 16:49:54 -0400

    Wiring up the details.html partial.

--- angular/js/controllers.js
+++ angular/js/controllers.js
@@ -6,3 +6,10 @@ artistControllers.controller('ListController', ['$scope', '$http', function($sco
 		$scope.artistOrder = 'name';
 	});
 }]);
+
+artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+	$http.get('js/data.json').then(function(response) {
+		$scope.artists = response.data;
+		$scope.whichItem = $routeParams.itemId;
+	});
+}]);
