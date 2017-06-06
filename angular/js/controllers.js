d1b79509dfde644a5307cc2a1fb32314d0acc8a6
Jack Tan <jack.ziqi.tan@gmail.com>
2017-06-06 11:56:06 -0400

    Added search and sorting to the list.

--- angular/js/controllers.js
+++ angular/js/controllers.js
@@ -3,5 +3,6 @@ var myApp = angular.module('myApp', []);
 myApp.controller('myController', ['$scope', '$http', function($scope, $http) {
 	$http.get('js/data.json').then(function(response) {
 		$scope.artists = response.data;
+		$scope.artistOrder = 'name';
 	});
 }]);
